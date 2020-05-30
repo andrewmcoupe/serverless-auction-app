4da39e7af1b344a983f8e5528de426dc8a3e8453
Andy Coupe <andrewmcoupe@gmail.com>
2020-05-30 09:03:12 +0100

    add prettier

--- src/auction/index.ts
+++ src/auction/index.ts
@@ -1,4 +1,4 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from 'uuid'
 
 export enum AuctionStatus {
   OPEN = 'OPEN',
@@ -6,19 +6,19 @@ export enum AuctionStatus {
 }
 
 export class Auction {
-  id: string;
-  title: string;
-  status: AuctionStatus;
-  createdAt: string;
+  id: string
+  title: string
+  status: AuctionStatus
+  createdAt: string
 
   constructor(title: string, status: AuctionStatus, createdAt: string) {
-    this.id = uuid();
-    this.title = title;
-    this.status = status;
-    this.createdAt = createdAt;
+    this.id = uuid()
+    this.title = title
+    this.status = status
+    this.createdAt = createdAt
   }
 
   updateStatus(status: AuctionStatus): void {
-    this.status = status;
+    this.status = status
   }
 }
--- src/handlers/createAuction.ts
+++ src/handlers/createAuction.ts
@@ -1,28 +1,28 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
-import { DynamoDB } from 'aws-sdk';
+import { APIGatewayProxyHandler } from 'aws-lambda'
+import 'source-map-support/register'
+import { DynamoDB } from 'aws-sdk'
 
-import { Auction, AuctionStatus } from '../auction';
+import { Auction, AuctionStatus } from '../auction'
 
-const dynamoDb = new DynamoDB.DocumentClient();
+const dynamoDb = new DynamoDB.DocumentClient()
 
 export const createAuction: APIGatewayProxyHandler = async (event, context) => {
-  const { title } = JSON.parse(event.body);
-  const now = new Date().toISOString();
-  const defaultStatus = AuctionStatus.OPEN;
+  const { title } = JSON.parse(event.body)
+  const now = new Date().toISOString()
+  const defaultStatus = AuctionStatus.OPEN
 
-  const auction: Auction = new Auction(title, defaultStatus, now);
+  const auction: Auction = new Auction(title, defaultStatus, now)
 
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Item: auction,
-  };
+  }
 
   // default put uses callbacks, chain on the promise method to use async await
-  await dynamoDb.put(params).promise();
+  await dynamoDb.put(params).promise()
 
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
-  };
-};
+  }
+}
