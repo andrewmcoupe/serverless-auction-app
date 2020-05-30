f5c448824996feac4eda6f28ffb3dafa84bcd3fc
Andy Coupe <andrewmcoupe@gmail.com>
2020-05-30 06:33:57 +0100

    add dynamodb to stack

--- src/handlers/createAuction.ts
+++ src/handlers/createAuction.ts
@@ -1,21 +1,41 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import { v4 as uuid } from 'uuid';
+import { DynamoDB } from 'aws-sdk';
+
+const dynamoDb = new DynamoDB.DocumentClient();
 
 enum AuctionStatus {
   OPEN = 'OPEN',
   CLOSED = 'CLOSED',
 }
 
+interface Auction {
+  id: string;
+  title: string;
+  status: AuctionStatus;
+  createdAt: string;
+}
+
 export const createAuction: APIGatewayProxyHandler = async (event, context) => {
   const { title } = JSON.parse(event.body);
   const now = new Date();
-  const defaultStatus: AuctionStatus = AuctionStatus.OPEN;
+  const defaultStatus = AuctionStatus.OPEN;
 
-  const auction = {
+  const auction: Auction = {
+    id: uuid(),
     title,
     status: defaultStatus,
     createdAt: now.toISOString(),
   };
+
+  const params = {
+    TableName: 'AuctionsTable',
+    Item: auction,
+  };
+
+  // default put uses callbacks, chain promise method to use async await
+  await dynamoDb.put(params).promise();
 
   return {
     statusCode: 201,
