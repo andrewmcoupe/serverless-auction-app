86fa146171ee4d0657d6168d41edaeaddb021297
Andy Coupe <andrewmcoupe@gmail.com>
2020-05-30 06:48:19 +0100

    add auction class

---
+++ src/auction/index.ts
@@ -0,0 +1,24 @@
+import { v4 as uuid } from 'uuid';
+
+export enum AuctionStatus {
+  OPEN = 'OPEN',
+  CLOSED = 'CLOSED',
+}
+
+export class Auction {
+  id: string;
+  title: string;
+  status: AuctionStatus;
+  createdAt: string;
+
+  constructor(title: string, status: AuctionStatus, createdAt: string) {
+    this.id = uuid();
+    this.title = title;
+    this.status = status;
+    this.createdAt = createdAt;
+  }
+
+  updateStatus(status: AuctionStatus): void {
+    this.status = status;
+  }
+}
--- src/handlers/createAuction.ts
+++ src/handlers/createAuction.ts
@@ -1,33 +1,17 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
-import { v4 as uuid } from 'uuid';
 import { DynamoDB } from 'aws-sdk';
+
+import { Auction, AuctionStatus } from '../auction';
 
 const dynamoDb = new DynamoDB.DocumentClient();
 
-enum AuctionStatus {
-  OPEN = 'OPEN',
-  CLOSED = 'CLOSED',
-}
-
-interface Auction {
-  id: string;
-  title: string;
-  status: AuctionStatus;
-  createdAt: string;
-}
-
 export const createAuction: APIGatewayProxyHandler = async (event, context) => {
   const { title } = JSON.parse(event.body);
-  const now = new Date();
+  const now = new Date().toISOString();
   const defaultStatus = AuctionStatus.OPEN;
 
-  const auction: Auction = {
-    id: uuid(),
-    title,
-    status: defaultStatus,
-    createdAt: now.toISOString(),
-  };
+  const auction: Auction = new Auction(title, defaultStatus, now);
 
   const params = {
     TableName: 'AuctionsTable',
