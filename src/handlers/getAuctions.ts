4fcafdcd00556e109a320c3f0fe6a522d884bf63
Andy Coupe <andrewmcoupe@gmail.com>
2020-05-31 08:04:50 +0100

    add get auctions handler

---
+++ src/handlers/getAuctions.ts
@@ -0,0 +1,33 @@
+import { APIGatewayProxyHandler } from 'aws-lambda'
+import { DynamoDB } from 'aws-sdk'
+import 'source-map-support/register'
+import middy from '@middy/core'
+import httpEventNormalizer from '@middy/http-event-normalizer'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
+
+const dynamoDb = new DynamoDB.DocumentClient()
+
+const handler: APIGatewayProxyHandler = async (event, context) => {
+  let auctions
+
+  try {
+    const result = await dynamoDb
+      .scan({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+      })
+      .promise()
+
+    auctions = result.Items
+  } catch (error) {
+    console.error(error)
+    throw new createError.InternalServerError(error)
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auctions),
+  }
+}
+
+export const getAuctions = middy(handler).use(httpEventNormalizer()).use(httpErrorHandler())
