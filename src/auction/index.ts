b765dd343ed099796e813421e1023278c062cde6
Andy Coupe <andrewmcoupe@gmail.com>
2020-06-03 07:44:41 +0100

    add endBy to auction

--- src/auction/index.ts
+++ src/auction/index.ts
@@ -14,14 +14,20 @@ export class Auction {
   title: string
   status: AuctionStatus
   highestBid: HighestBid
+  endingAt: string
   createdAt: string
 
   constructor() {
+    const now = new Date()
+    const endDate = new Date()
+    endDate.setHours(now.getHours() + 1) // sets auction duration to 1 hour
+
     this.id = uuid()
     this.status = AuctionStatus.OPEN
     this.highestBid = {
       amount: 0,
     }
-    this.createdAt = new Date().toISOString()
+    this.createdAt = now.toISOString()
+    this.endingAt = endDate.toISOString()
   }
 }
---
+++ src/handlers/processAuctions.ts
@@ -0,0 +1,5 @@
+const processAuctions = async (event, context) => {
+  console.log('processing auctions')
+}
+
+export const handler = processAuctions
