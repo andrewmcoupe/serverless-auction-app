deab8b34969739bc870178964783398153f99db4
Andy Coupe <andrewmcoupe@gmail.com>
2020-06-17 07:00:24 +0100

    add request validation

--- src/handlers/getAuctions.ts
+++ src/handlers/getAuctions.ts
@@ -2,21 +2,36 @@ import { APIGatewayProxyHandler } from 'aws-lambda'
 import { DynamoDB } from 'aws-sdk'
 import 'source-map-support/register'
 import createError from 'http-errors'
+import validator from '@middy/validator'
 import commonMiddleware from '../lib/commonMiddleware'
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema'
 
 const dynamoDb = new DynamoDB.DocumentClient()
 
 const getAuctions: APIGatewayProxyHandler = async (event, context) => {
-  let auctions: DynamoDB.DocumentClient.ItemList
+  let auctions: DynamoDB.DocumentClient.ItemList | undefined
+  const status = event.queryStringParameters?.status
+
+  if (!status) {
+    throw new createError.BadRequest('Status of OPEN or CLOSED must be provided as a query string')
+  }
+
+  const params: DynamoDB.DocumentClient.QueryInput = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndingAt',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  }
 
   try {
-    const result = await dynamoDb
-      .scan({
-        TableName: process.env.AUCTIONS_TABLE_NAME,
-      })
-      .promise()
+    const result = await dynamoDb.query(params).promise()
 
-    auctions = result.Items
+    auctions = result?.Items
   } catch (error) {
     console.error(error)
     throw new createError.InternalServerError(error)
@@ -28,4 +43,6 @@ const getAuctions: APIGatewayProxyHandler = async (event, context) => {
   }
 }
 
-export const handler = commonMiddleware(getAuctions)
+export const handler = commonMiddleware(getAuctions).use(
+  validator({ inputSchema: getAuctionsSchema }),
+)
